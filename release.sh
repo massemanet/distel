36dc477f35e83610915b99df09ca402b5d4cbef3
mats cronqvist <masse@klarna.com>
2014-02-25 18:38:46 +0100

    add release.sh script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+OVSN=`git tag | head -1`
+NVSN=`echo $OVSN | cut -f1 -d"."`.$((`echo $OVSN | cut -f2 -d"."` + 1))
+echo $APPSRC $OVSN"->"$NVSN
+sed  s/$OVSN/$NVSN/ < $APPSRC > $$ && mv $$ $APPSRC
+git add $APPSRC
+git commit -m"v$NVSN"
+git log --name-only --no-merges | grep -Ev '^[ ]+$$|git-svn-id' > ChangeLog
+echo " Mats Cronqvist <masse@cronqvi.st>" > AUTHORS
+git log | grep Author | grep -Evi "vagrant|no author|mats cronqvist" \
+  | sort -u | cut -c8- >> AUTHORS
+git add ChangeLog AUTHORS
+git commit --amend --reuse-message HEAD
+git tag -a -m"$NVSN" $NVSN
+#git push --tags
