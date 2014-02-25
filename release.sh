8815839b69044cee67f15c3c393b74537a36a323
mats cronqvist <masse@klarna.com>
2014-02-25 18:43:26 +0100

    fix release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+function usage () {
+    echo "$0 major|minor|patch"
+    exit
+}
+
+if [ -z "$1" ]; then
+    size="patch"
+else
+    size=$1
+fi
 
 OVSN=`git tag | head -1`
-NVSN=`echo $OVSN | cut -f1 -d"."`.$((`echo $OVSN | cut -f2 -d"."` + 1))
-echo $APPSRC $OVSN"->"$NVSN
-sed  s/$OVSN/$NVSN/ < $APPSRC > $$ && mv $$ $APPSRC
-git add $APPSRC
+MAJOR=`echo $OVSN | cut -f1 -d"."`
+MINOR=`echo $OVSN | cut -f2 -d"."`
+PATCH=`echo $OVSN | cut -f3 -d"."`
+if [ "$size" == "major" ]; then
+    NVSN=$(($MAJOR + 1)).0.0
+elif [ "$size" == "minor" ]; then
+    NVSN=$MAJOR.$(($MINOR + 1)).0
+elif [ "$size" == "patch" ]; then
+    NVSN=$MAJOR.$MINOR.$(($PATCH + 1))
+else
+    usage
+fi
+
+echo $OVSN"->"$NVSN
 git commit -m"v$NVSN"
 git log --name-only --no-merges | grep -Ev '^[ ]+$$|git-svn-id' > ChangeLog
 echo " Mats Cronqvist <masse@cronqvi.st>" > AUTHORS
