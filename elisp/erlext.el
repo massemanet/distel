d2cefa5c4d61212c4c11774ca6d5b7588bad1eeb
Leo Liu <sdl.web@gmail.com>
2014-05-22 15:48:40 +0800

    erlext.el: Add support for float and new_float
    Rename empty-symbol to erlext-empty-symbol.

--- elisp/erlext.el
+++ elisp/erlext.el
@@ -14,12 +14,13 @@
 ;;   atom    -> symbol
 ;;   string  -> string
 ;;   integer -> integer
+;;   float   -> float
 ;;   list    -> list
 ;;   tuple   -> (vector ...)
 ;;   pid     -> (vector ERL-TAG 'pid node id serial creation)
 ;;   binary  -> string
 ;; Not mapped/supported yet:
-;;   ref, port, float, bignum, function, ...
+;;   ref, port, bignum, function, ...
 ;;
 ;; ----------------------------------------------------------------------
 ;; Revision history:
@@ -34,18 +35,18 @@
 ;; March 2002: Big cleanup for use in distributed erlang. Removed the
 ;; old networking code.
 
-(eval-when-compile (require 'cl))
-(eval-when-compile (load "cl-extra"))
+(require 'cl)
 
 ;; type tags
 
 (defconst erlext-tag-alist
-  '((smallInt   . 97)
+  '((newFloat   . 70)
+    (smallInt   . 97)
     (int        . 98)
-    (float      . 99)
+    (float      . 99)                   ;superseded by newFloat
     (atom       . 100)
     (cached     . 67)
-    (ref        . 101)
+    (ref        . 101)                  ;superseded by newRef
     (port       . 102)
     (pid        . 103)
     (smallTuple . 104)
@@ -61,7 +62,7 @@
 (defconst erlext-max-atom-length 255 "The maximum length of an erlang atom.")
 (defconst erlext-protocol-version 131)
 
-(defconst empty-symbol (intern "")
+(defconst erlext-empty-symbol (intern "")
   "The zero-length lisp symbol.")
 
 (defvar erl-tag (make-symbol "TYPE")
@@ -140,6 +141,8 @@
                 (erlext-write-binary (car elts)))))))
         ((integerp obj)
          (erlext-write-int obj))
+        ((floatp obj)
+         (erlext-write-float obj))
         (t
          (error "erlext can't marshal %S" obj))))
 
@@ -172,6 +175,7 @@
     (erlext-write1 (erlext-get-code 'atom))
     (erlext-write2 (length string))
     (erlext-writen string)))
+
 (defun erlext-write-int (n)
   (assert (integerp n))
   (cond ((= n (logand n 255))
@@ -182,6 +186,65 @@
         (t
          (erlext-write1 (erlext-get-code 'int))
          (erlext-write4 n))))
+
+;; Test erlext-encode-ieee-double
+;;
+;; (cl-mapcar (lambda (x y) (equal (erlext-encode-ieee-double x) y))
+;;            '(1e+INF -1e+INF -12.2 1.0000000000000004)
+;;            '((127 240 0 0 0 0 0 0)
+;;              (255 240 0 0 0 0 0 0)
+;;              (192 40 102 102 102 102 102 102)
+;;              (63 240 0 0 0 0 0 2)))
+;; => (t t t t)
+(defun erlext-encode-ieee-double (n)
+  ;; Ref: http://en.wikipedia.org/wiki/Double-precision_floating-point_format
+  (labels ((fill-mantissa (vec frac)
+             (loop for i from 12 to 63
+                   for tmp = (- frac (expt 0.5 (- i 11)))
+                   when (>= tmp 0)
+                   do (setf (aref vec i) (prog1 1 (setq frac tmp)))))
+           (bytes (bits)
+             (loop for i from 0 to 63
+                   for b = 7 then (1- b)
+                   sum (* (aref bits i) (expt 2 b)) into byte
+                   when (zerop b)
+                   collect (prog1 byte (setf byte 0 b 8)))))
+    (let* ((result (make-vector 64 0))
+           (bias 1023)
+           (E (frexp n))
+           (S (pop E)))
+      ;; Sign
+      (when (< S 0)
+        (setf (aref result 0) 1))
+      ;; Exponent & Mantissa
+      (cond ((isnan n) (fill result 1 :start 1 :end 64))
+            ((member S '(1.0e+INF -1.0e+INF))
+             (fill result 1 :start 1 :end 12)
+             (fill result 0 :start 12 :end 64))
+            ((zerop E)                  ; subnormals
+             (fill result 0 :start 1 :end 12)
+             (fill-mantissa result S))
+            ;; Move factor 2 to S so that S >= 1.0
+            (t (loop with x = (+ (1- E) bias)
+                     for i from 11 downto 1
+                     do (setf (aref result i) (prog1 (logand x 1)
+                                                (setq x (ash x -1)))))
+               (fill-mantissa result (1- (* 2 (abs S))))))
+      (bytes result))))
+
+(defun erlext-write-float (n)
+  (cond
+   ((fboundp 'frexp)
+    ;; Function `frexp' was introduced in emacs 24.1;
+    ;; Erlang R11B-4 and later is able decode this representation.
+    (erlext-write1 (erlext-get-code 'newFloat))
+    (mapc #'erlext-write1 (erlext-encode-ieee-double n)))
+   (t
+    (erlext-write1 (erlext-get-code 'float))
+    (let ((f (format "%.20e" n)))
+      (erlext-writen (concat f (make-string
+                                (max 0 (- 31 (length f))) 0)))))))
+
 (defun erlext-write-list (lst)
   (assert (listp lst))
   (if (null lst)
@@ -261,6 +324,8 @@
     (case tag
       ((smallInt)   (erlext-read1))
       ((int)        (erlext-read4))
+      ((newFloat)   (erlext-read-ieee-double))
+      ((float)      (erlext-read-float))
       ((atom)       (erlext-read-atom))
       ((smallTuple) (erlext-read-small-tuple))
       ((largeTuple) (erlext-read-large-tuple))
@@ -272,24 +337,24 @@
       ((pid)        (vector erl-tag
                             'erl-pid
                             (erlext-read-obj) ; node
-                            (erlext-read4) ; id
-                            (erlext-read4) ; serial
-                            (erlext-read1))); creation
+                            (erlext-read4)    ; id
+                            (erlext-read4)    ; serial
+                            (erlext-read1)))  ; creation
       ((port)       (vector erl-tag
                             'erl-port
                             (erlext-read-obj) ; node
-                            (erlext-read4) ; id
-                            (erlext-read1))) ; creation
+                            (erlext-read4)    ; id
+                            (erlext-read1)))  ; creation
       ((ref)        (vector erl-tag
                             'erl-ref
                             (erlext-read-obj) ; node
-                            (erlext-read4) ;id
-                            (erlext-read1))) ; creation
+                            (erlext-read4)    ; id
+                            (erlext-read1)))  ; creation
       ((newRef)     (erlext-read-newref))
       ((smallBig)   (erlext-read-small-bignum))
       ((largeBig)   (erlext-read-large-bignum))
-     (t
-      (error "Unknown tag: %S" tag)))))
+      (t
+       (error "Unknown tag: %S" tag)))))
 
 (defun erlext-read (size)
   (case size
@@ -305,6 +370,7 @@
           (ash (erlext-read1) 16)
           (ash (erlext-read1) 8)
           (erlext-read1)))
+
 (defun erlext-readn (n)
   (assert (integerp n))
   (let ((start (point))
@@ -317,6 +383,42 @@
                                             ; Why is it necessary
                                             ; anyhow?
       (goto-char end))))
+
+;; Test:
+;; (cl-labels ((test (x)
+;;               (with-temp-buffer
+;;                 (set-buffer-multibyte nil)
+;;                 (apply #'insert (erlext-encode-ieee-double x))
+;;                 (goto-char (point-min))
+;;                 (equal (erlext-read-ieee-double) x))))
+;;   (mapcar #'test '(1.0e+INF -1.0e+INF -12.2 1.0000000000000004)))
+;; => (t t t t)
+(defun erlext-read-ieee-double ()
+  (labels ((to-bits (byte)
+             (nreverse (loop repeat 8
+                             collect (prog1 (logand byte 1)
+                                       (setq byte (ash byte -1)))))))
+    (let* ((bias 1023)
+           (bits (apply #'vector (loop repeat 8
+                                       append (to-bits (erlext-read1)))))
+           (sign (if (zerop (aref bits 0)) 1 -1))
+           (exponent (loop for i from 11 downto 1
+                           sum (if (zerop (aref bits i))
+                                   0
+                                 (ash 1 (- 11 i)))))
+           (fraction (loop for i from 12 to 63
+                           sum (* (aref bits i)
+                                  (expt 0.5 (- i 11))))))
+      (case exponent
+        (#x7ff (if (zerop fraction)
+                   (* sign 1.0e+INF)
+                 0.0e+NaN))
+        (0 (* sign (expt 2 (1- bias)) fraction))
+        (t (* sign (expt 2 (- exponent bias)) (1+ fraction)))))))
+
+(defun erlext-read-float ()
+  (string-to-number (erlext-readn 31)))
+
 (defun erlext-read-atom ()
   (let ((length (erlext-read2)))
     (intern (erlext-readn length))))
